defe8f3d38ac905f3df78e85c4e63bf4a448d6d2
Isaac Chacko <isaac.chacko05@gmail.com>
2025-03-11 17:27:47 -0500

    so much for 'type' script

--- src/app/api/strava/cache.ts
+++ src/app/api/strava/cache.ts
@@ -1,23 +1,32 @@
 import NodeCache from 'node-cache';
 
+interface Cache {
+  set(key: string, value: string): void;
+  get(key: string): string | undefined;
+}
+
+declare global {
+  var cache: Cache | undefined;
+}
+
 const cache = global.cache || (global.cache = new NodeCache({ stdTTL: 21600 })); // Cache expires in 6 hours
 
-export function setAccessToken(token) {
+export function setAccessToken(token: string) {
   console.log('Saving access token...');
   cache.set('stravaAccessToken', token);
 }
 
-export function setRefreshToken(token) {
+export function setRefreshToken(token: string) {
   console.log('Saving refresh token...');
   cache.set('stravaRefreshToken', token);
 }
 
-export function getAccessToken() {
+export function getAccessToken(): string | undefined {
   console.log('Retrieving access token...');
   return cache.get('stravaAccessToken');
 }
 
-export function getRefreshToken() {
+export function getRefreshToken(): string | undefined {
   console.log('Retrieving refresh token...');
   return cache.get('stravaRefreshToken');
 }
