f9d0e5ec3eabb5b88b365c35b5c7588b1a27a68f
Isaac Chacko <isaac.chacko05@gmail.com>
2025-03-11 17:46:57 -0500

    2x i hate this

--- src/app/api/strava/callback/route.ts
+++ src/app/api/strava/callback/route.ts
@@ -38,7 +38,11 @@ export async function GET(request: NextRequest) {
 
     // Return success response
     return NextResponse.json({ message: 'Tokens saved successfully', access_token: data.access_token });
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    } else {
+      return NextResponse.json({ error: 'Unknown error occurred' }, { status: 500 });
+    }
   }
 }
