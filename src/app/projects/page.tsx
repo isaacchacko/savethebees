9957e4689620605a3e201357b37bdf69c3a85ef5
Isaac Chacko <isaac.chacko05@gmail.com>
2025-05-14 17:21:08 -0500

    add eulerelo boilerplate, arrow topright link

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,6 +6,9 @@ import CanvasBackground from '@/components/CanvasBackground';
 import useResponsiveBees from '@/hooks/useResponsiveBees';
 import Navbar from "@/components/Navbar";
 
+// for the external links
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline';
+
 export default function Home() {
   const numBees = useResponsiveBees();
   const spawnRadius = 100;
@@ -20,49 +23,48 @@ export default function Home() {
       <div className="pointer-events-none relative flex flex-col min-h-screen justify-start">
         <Navbar showColorPalette={false} learnMorePath="/tracking" />
 
-        <div className="slide-down-fade-in flex flex-col w-full max-w-4xl mx-auto p-4 md:p-8 my-8 backdrop-blur-sm relative bg-(--spotify-background) rounded-lg shadow">
-          <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-8">
-            {/* About Me Text */}
-            <div className="pointer-events-auto text-base leading-relaxed text-center md:text-left">
-              <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-(--primary-color) font-black pb-1 md:pb-6">
-                In Progress!
+        <div className="slide-down-fade-in">
+          <div className="pointer-events-auto flex flex-col w-full max-w-4xl mx-auto p-4 md:p-8 mt-8 backdrop-blur-sm relative bg-(--spotify-background) rounded-lg shadow">
+            <div className="text-base leading-relaxed text-center md:text-left">
+              <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-(--primary-color) font-black pb-1 md:pb-6"> Projects
               </h2>
               <p className="text-sm sm:text-base md:text-lg lg:text-xl">
-                While you&apos;re here, how about you find some bugs for me! Let me know what you find.
-              </p>
-            </div>
-
-            {/* Flip Card Container */}
-            <div
-              className="pointer-events-auto relative perspective w-[250px] sm:w-[400px] md:w-[600px] lg:w-[800px]"
-              onClick={() => setIsFlipped(!isFlipped)}
-            >
-              {/* Inner Card */}
-              <div
-                className={`transition-transform duration-500 transform-style-preserve-3d ${
-                  isFlipped ? 'rotate-y-180' : ''
-                }`}
-              >
-                {/* Front Side (Image) */}
-                <div className="backface-hidden">
-                  <Image
-                    src="/profile-picture.jpg"
-                    alt="Your Name"
-                    width={1500}
-                    height={1500}
-                    className="rounded-lg object-cover scale-90 hover:scale-100 transition-transform duration-300 ring-3 ring-(--tertiary-color)"
-                  />
-                </div>
-
-                {/* Back Side (Blurb Text) */}
-                <div className="absolute inset-0 bg-(--tertiary-color) rounded-lg flex items-center justify-center backface-hidden rotate-y-180">
-                  <p className="text-center px-4 text-sm sm:text-base md:text-lg lg:text-xl">
-                    BOO!
-                  </p>
-                </div>
-              </div>
+  Although a complete list can be found on my <a target="_blank" href='https://github.com/isaacchacko' rel="noopener noreferrer" className="text-(--primary-color) underline underline-offset-2 hover:text-(--tertiary-color)">GitHub</a>, here&apos;s a brief list of my more notable projects.
+</p>
             </div>
           </div>
+
+          <div className="pointer-events-auto flex flex:w-col w-full max-w-4xl mx-auto p-4 md:p-8 mt-8 backdrop-blur-sm relative bg-(--spotify-background) rounded-lg shadow">
+            <div className="text-base leading-relaxed">
+<div className="flex flex-row jusitfy-center items">
+
+              <h2   className="font-black text-white text-2xl 2xl:text-4xl text-white text-bold cursor-pointer pb-2">
+<a href="http://eulerelo.vercel.app" target="_blank" rel="noopener noreferrer" className="underline underline-offset-5 hover:text-(--primary-color)">
+
+Eulerelo
+</a>
+
+              </h2>
+<ArrowTopRightOnSquareIcon className="ml-1 h-4 w-4" aria-label="Opens in new tab" />
+</div>
+              <p className="text-sm sm:text-base md:text-lg lg:text-xl">
+Challenge your math skills in real-time duels, climb the leaderboard, and master topics from arithmetic to calculus. Compete with others, solve problems faster, and see how you rank among math enthusiasts worldwide!
+              </p>
+            </div>
+          </div>
+
+
+          <div className="pointer-events-auto flex flex-col w-full max-w-4xl mx-auto p-4 md:p-8 mt-8 backdrop-blur-sm relative bg-(--spotify-background) rounded-lg shadow">
+            <div className="text-base leading-relaxed">
+              <h2   className="font-black text-white text-2xl 2xl:text-4xl text-white cursor-pointer pb-2">
+                Under construction...
+              </h2>
+              <p className="text-sm sm:text-base md:text-lg lg:text-xl">
+                I'll eventually cover this page with any metric you can think of. Red Bulls downed, late night hours grinded, my CS in League-- you name it.
+              </p>
+            </div>
+          </div>
+
         </div>
 
         <footer className="absolute bottom-2 left-2 contactInfo">
