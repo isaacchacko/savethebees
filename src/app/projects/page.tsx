4135c065c533650e97c82c7282444dab76c31696
Isaac Chacko <isaac.chacko05@gmail.com>
2025-05-16 22:01:02 -0500

    fix indentation

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -29,26 +29,26 @@ export default function Home() {
               <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-(--primary-color) font-black pb-1 md:pb-6"> Projects
               </h2>
               <p className="text-sm sm:text-base md:text-lg lg:text-xl">
-  Although a complete list can be found on my <a target="_blank" href='https://github.com/isaacchacko' rel="noopener noreferrer" className="text-(--primary-color) underline underline-offset-2 hover:text-(--tertiary-color)">GitHub</a>, here&apos;s a brief list of my more notable projects.
-</p>
+                Although a complete list can be found on my <a target="_blank" href='https://github.com/isaacchacko' rel="noopener noreferrer" className="text-(--primary-color) underline underline-offset-2 hover:text-(--tertiary-color)">GitHub</a>, here&apos;s a brief list of my more notable projects.
+              </p>
             </div>
           </div>
 
           <div className="pointer-events-auto flex flex:w-col w-full max-w-4xl mx-auto p-4 md:p-8 mt-8 backdrop-blur-sm relative bg-(--spotify-background) rounded-lg shadow">
             <div className="text-base leading-relaxed">
-<div className="flex flex-row jusitfy-center items">
+              <div className="flex flex-row jusitfy-center items">
 
-              <h2   className="font-black text-white text-2xl 2xl:text-4xl text-white text-bold cursor-pointer pb-2">
-<a href="http://eulerelo.vercel.app" target="_blank" rel="noopener noreferrer" className="underline underline-offset-5 hover:text-(--primary-color)">
+                <h2   className="font-black text-white text-2xl 2xl:text-4xl text-white text-bold cursor-pointer pb-2">
+                  <a href="http://eulerelo.vercel.app" target="_blank" rel="noopener noreferrer" className="underline underline-offset-5 hover:text-(--primary-color)">
 
-Eulerelo
-</a>
+                    Eulerelo
+                  </a>
 
-              </h2>
-<ArrowTopRightOnSquareIcon className="ml-1 h-4 w-4" aria-label="Opens in new tab" />
-</div>
+                </h2>
+                <ArrowTopRightOnSquareIcon className="ml-1 h-4 w-4" aria-label="Opens in new tab" />
+              </div>
               <p className="text-sm sm:text-base md:text-lg lg:text-xl">
-Challenge your math skills in real-time duels, climb the leaderboard, and master topics from arithmetic to calculus. Compete with others, solve problems faster, and see how you rank among math enthusiasts worldwide!
+                Challenge your math skills in real-time duels, climb the leaderboard, and master topics from arithmetic to calculus. Compete with others, solve problems faster, and see how you rank among math enthusiasts worldwide!
               </p>
             </div>
           </div>
